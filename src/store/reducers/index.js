fafa651b223cdc2e75188cbbdccd3676c90f13e5
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-01-10 16:40:59 -0300

    (feat: store) Implementada  Store Generica

---
+++ src/store/reducers/index.js
@@ -0,0 +1,13 @@
+// import user from './user';
+// import wallet from './wallet';
+
+// Configure os seus reducers.
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const reducer = (state, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default reducer;
