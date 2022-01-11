36641516128c0ee7086cc704138f87b7f7731574
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-01-11 00:49:13 -0300

    (build: Header) implementado a prop total para receber o valor do store

---
+++ src/components/Header/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import trybeLogo from '../../images/trybe_logo.png';
+import './style.css';
+
+export class Header extends Component {
+  render() {
+    const { email, total } = this.props;
+    return (
+      <header className="header">
+        <div className="header-content">
+          <img className="logo" src={ trybeLogo } alt="Logo Trybe" />
+          <div className="user-email">
+            <p data-testid="email-field">{email}</p>
+          </div>
+          <p
+            data-testid="total-field"
+          >
+            <span data-testid="total-field">{ `Despesas Totais ${total}` }</span>
+          </p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string,
+  total: PropTypes.string,
+};
+
+Header.defaultProps = {
+  email: '',
+  total: '',
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
